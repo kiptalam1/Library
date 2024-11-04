d5f824b6f251d2af5c60664fe4cb2a95d886916c
adams <adamskiptalam0@gmail.com>
2024-11-04 16:30:45 +0300

    Add pages, and isRead parameters to the Book.

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const myLibrary = [];
 
-function Book (name, author, year) {
+function Book (name, author, pages, year, isRead) {
     this.name = name;
     this.author = author;
+    this.pages = pages;
     this.year = year;
-
+    this.isRead = isRead;
 }
 
 function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 
-const book1 = new Book("river and the source", "Margaret Ogolla", "2000"); 
+const book1 = new Book("river and the source", "Margaret Ogolla", 400, "2000", true); 
 addBookToLibrary(book1);
 console.log(book1);
 console.log(myLibrary);
