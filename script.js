9cad8953f6c8620eb3265aa4ab8b656f8ea7d317
adams <adamskiptalam0@gmail.com>
2024-11-04 17:06:45 +0300

    Add the logic to display books in table format.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const myLibrary = [];
+const myLibrary = [
+    { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, year: 1960, isRead: true },
+    { title: "Pride and Prejudice", author: "Jane Austen", pages: 279, year: 1813, isRead: false },
+    { title: "The Catcher in the Rye", author: "J.D. Salinger", pages: 214, year: 1951, isRead: true },
+    { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, year: 1937, isRead: false },
+    { title: "1984", author: "George Orwell", pages: 328, year: 1949, isRead: true },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, year: 1925, isRead: false }
+];  
 
-function Book (name, author, pages, year, isRead) {
-    this.name = name;
+
+function Book (title, author, pages, year, isRead) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.year = year;
@@ -12,7 +20,26 @@ function addBookToLibrary (book) {
     myLibrary.push(book);
 }
 
-const book1 = new Book("river and the source", "Margaret Ogolla", 400, "2000", true); 
-addBookToLibrary(book1);
-console.log(book1);
-console.log(myLibrary);
+function displayBooksTable () {
+    const tableBody = document.getElementById('libraryTable').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+    myLibrary.forEach((book) => {
+        const row = document.createElement('tr');
+
+        for (const key in book) {
+            const cell = document.createElement('td');
+            cell.textContent = book[key];
+            row.appendChild(cell);
+        }
+        tableBody.appendChild(row);
+    });
+}
+
+displayBooksTable();
+
+
+
+// const book1 = new Book("river and the source", "Margaret Ogolla", 400, "2000", true); 
+// addBookToLibrary(book1);
+// console.log(book1);
+// console.log(myLibrary);
