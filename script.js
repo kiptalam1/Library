43589a7f8ea4b5d82d8f76e133d338d6218aa768
adams <adamskiptalam0@gmail.com>
2024-11-18 20:13:07 +0300

    Remove unnecessary commented code

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// const myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [
-//     { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, year: 1960, isRead: true },
-//     { title: "Pride and Prejudice", author: "Jane Austen", pages: 279, year: 1813, isRead: false },
-//     { title: "The Catcher in the Rye", author: "J.D. Salinger", pages: 214, year: 1951, isRead: true },
-//     { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, year: 1937, isRead: false },
-//     { title: "1984", author: "George Orwell", pages: 328, year: 1949, isRead: true },
-//     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, year: 1925, isRead: false }
-// ];
 
 const newBookButton = document.getElementById('new-book');
 const dialog = document.querySelector('dialog');
